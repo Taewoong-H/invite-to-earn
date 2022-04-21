a8fdfb00d9be6048ebd3afbbbc2baf0c1c36bc42
Taewoong-H <taeung2008@gmail.com>
2022-04-21 22:39:22 +0900

    style: 홈화면 css 디자인 따라 구현 간단하게 1차 디자인

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Home = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-md navbar-light bg-white absolute-top">
+      <nav className="navbar navbar-expand-md bg-white mt-2">
         <div className="container">
-          <h2 className="navbar-brand">INVITE 2 EARN</h2>
+          <h2 className="navbar-brand logo">INVITE 2 EARN</h2>
           <div>
             <ul className="navbar-nav ms-auto">
               <li className="nav-item text-center">
@@ -29,8 +29,16 @@ const Home = () => {
       </nav>
       <body>
         <div className="container">
-          <h2>링크 등록하고,</h2>
-          <h2>필요한 링크 찾아가고</h2>
+          <div className="banner-container d-inline-block">
+            <h1 className="banner text-start px-2">
+              <span className="deco">링크 등록</span>하고, <br /> 필요한 <span className="deco">링크 찾아가고</span>
+            </h1>
+            <p className="banner-sm mt-4">
+              필요한 링크를 검색해서 포인트를 얻어가거나 본인의 링크를 등록한 후<br />
+              다른 유저를 초대하여 포인트 얻어가세요!
+            </p>
+            <input type="text" className="search mt-4"></input>
+          </div>
         </div>
       </body>
     </>
--- src/pages/TestSearch.js
+++ src/pages/TestSearch.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
 
 const searchClient = algoliasearch('W1ILN24TP3', 'cf48b00fb24a0df99c1d79856e21cd62');
 
-const startSearch = (event) => {
-  event.preventDefault();
-  console.log('startSearch');
-};
-
 const TestSearch = () => {
+  const [isHit, setIsHit] = useState(false);
+  const startSearch = (event) => {
+    event.preventDefault();
+    setIsHit(true);
+    console.log(isHit);
+  };
   return (
     <InstantSearch searchClient={searchClient} indexName="dev_services">
       <SearchBox onSubmit={startSearch} />
-      <Hits />
+      {isHit ? <Hits /> : 'dd'}
+      {/* <Hits /> */}
     </InstantSearch>
   );
 };
