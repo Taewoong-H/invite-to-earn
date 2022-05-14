632a14b77d463d212060133c63c5d21c7decd85e
Taewoong-H <taeung2008@gmail.com>
2022-05-14 19:08:03 +0900

    Updates

---
+++ precache-manifest.d20a473c0719e379dd0f63c4ee30e05d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "866f09865076a1c377b5",
+    "url": "/static/css/main.41d8bf67.chunk.css"
+  },
+  {
+    "revision": "866f09865076a1c377b5",
+    "url": "/static/js/main.866f0986.chunk.js"
+  },
+  {
+    "revision": "707a71b515e55ab9c6fe",
+    "url": "/static/js/1.707a71b5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c1cf19644a820e39abaa037674cf9763",
+    "url": "/index.html"
+  }
+];
