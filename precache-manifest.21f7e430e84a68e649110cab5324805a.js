2a83d7d70567f4fe23b5b91c8639bacebf982135
Taewoong-H <taeung2008@gmail.com>
2022-05-14 19:51:52 +0900

    Updates

---
+++ precache-manifest.21f7e430e84a68e649110cab5324805a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f44b5d8b2ced0413791e",
+    "url": "/invite-to-earn/static/css/main.a1a33b40.chunk.css"
+  },
+  {
+    "revision": "f44b5d8b2ced0413791e",
+    "url": "/invite-to-earn/static/js/main.f44b5d8b.chunk.js"
+  },
+  {
+    "revision": "707a71b515e55ab9c6fe",
+    "url": "/invite-to-earn/static/js/1.707a71b5.chunk.js"
+  },
+  {
+    "revision": "afa25422e9fdc4df6cf1",
+    "url": "/invite-to-earn/static/js/runtime~main.afa25422.js"
+  },
+  {
+    "revision": "a3cbe88bb6935278d18c46b5004972df",
+    "url": "/invite-to-earn/index.html"
+  }
+];
